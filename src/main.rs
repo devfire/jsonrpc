0971bd30b1c1df238c031db86f4467a51e493bb9
Igor Kantor <igor@devfire.io>
2025-03-26 23:09:05 -0400

    added more objects

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 mod objects;
 
+use actix_web::{App, HttpServer, Responder, web};
 use objects::JsonRpcRequest;
 
-fn main() {
-    println!("Hello, world!");
+async fn handle_json(data: web::Json<JsonRpcRequest>) -> impl Responder {
+    println!("Received JSON: {:?}", data);
+    "Data received"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::post().to(handle_json)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
--- src/objects.rs
+++ src/objects.rs
@@ -1,13 +1,32 @@
 // contains the JSON RPC objects that are used to communicate with the server.
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 // fix the jsonrpc string to 2.0 
-const JSONRPC_VERSION: &str ="2.0";
+// const JSONRPC_VERSION: &str ="2.0";
 
 #[derive(Deserialize, Debug)]
-pub struct JsonRpcRequest {
+pub(crate) struct JsonRpcRequest {
     jsonrpc: String,
     method: String,
-    params: String,
-    id: u32,
+    params: Value, // Use Value for flexibility (can be array, object, null)
+    id: Option<Value>, // Can be string, number, or null. Option handles notification case (id=null)
+}
+
+#[derive(Serialize, Debug)]
+struct JsonRpcError {
+    code: i32,
+    message: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    data: Option<Value>,
+}
+
+#[derive(Serialize, Debug)]
+struct JsonRpcResponse {
+    jsonrpc: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    result: Option<Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    error: Option<JsonRpcError>,
+    id: Option<Value>,
 }
